cc8d0893c5ee996798cddddf625cec99873f22b1
k-bett <Kiprotichemmanuel415@gmail.com>
2025-03-06 22:34:55 +0300

    Update Movies.js

--- src/Movies.js
+++ src/Movies.js
@@ -23,54 +23,21 @@ export default class Movies extends Component {
 
   state = {
     popupIsOpen: false,
-    // Day chosen by user
-    chosenDay: 0,       // choose first day by default
-    // Time chosen by user
-    chosenTime: null,
+    currentVideo: null,  // Add currentVideo state
   }
 
   openMovie = (movie) => {
     this.setState({
       popupIsOpen: true,
-      movie,
+      currentVideo: movie.videoUrl,  // Set the current video URL
     });
   }
 
   closeMovie = () => {
     this.setState({
       popupIsOpen: false,
-      // Reset values to default ones
-      chosenDay: 0,
-      chosenTime: null,
+      currentVideo: null,  // Reset the current video URL
     });
-  }
-
-  chooseDay = (day) => {
-    this.setState({
-      chosenDay: day,
-    });
-  }
-
-  chooseTime = (time) => {
-    this.setState({
-      chosenTime: time,
-    });
-  }
-
-  bookTicket = () => {
-    // Make sure they selected time
-    if (!this.state.chosenTime) {
-      alert('Please select show time');
-    } else {
-      // Close popup
-      this.closeMovie();
-      // Navigate away to Confirmation route
-      this.props.navigator.push({
-        name: 'confirmation',
-        // Generate random string
-        code: Math.random().toString(36).substring(6).toUpperCase(),
-      });
-    }
   }
 
   render() {
@@ -80,16 +47,7 @@ export default class Movies extends Component {
         {movies
           ? <ScrollView
               contentContainerStyle={styles.scrollContent}
-              // Hide all scroll indicators
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}
-              refreshControl={
-                <RefreshControl
-                  refreshing={loading}
-                  onRefresh={refresh}
-                />
-              }
-            >
+              >
               {movies.map((movie, index) => <MoviePoster
                 movie={movie}
                 onOpen={this.openMovie}
@@ -103,14 +61,9 @@ export default class Movies extends Component {
             />
         }
         <MoviePopup
-          movie={this.state.movie}
+          videoUrl={this.state.currentVideo}  // Pass the current video URL
           isOpen={this.state.popupIsOpen}
           onClose={this.closeMovie}
-          chosenDay={this.state.chosenDay}
-          chosenTime={this.state.chosenTime}
-          onChooseDay={this.chooseDay}
-          onChooseTime={this.chooseTime}
-          onBook={this.bookTicket}
         />
       </View>
     );
@@ -119,16 +72,16 @@ export default class Movies extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,                // take up all screen
-    paddingTop: 20,         // start below status bar
+    flex: 1,
+    paddingTop: 20,
   },
   loader: {
     flex: 1,
-    alignItems: 'center',     // center horizontally
-    justifyContent: 'center', // center vertically
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   scrollContent: {
-    flexDirection: 'row',   // arrange posters in rows
-    flexWrap: 'wrap',       // allow multiple rows
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
